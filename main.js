0ea3259ff109721838fc347c826e2679fb1e8b5f
Benjamin <benjbruce778@gmail.com>
2023-03-08 01:06:05 +0000

    Moon added to page

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import * as THREE from 'three';
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+var s = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera(75, s.width / s.height, 0.1, 1000);
+camera.position.setZ(30);
+scene.add(camera);
+
+const renderer = new THREE.WebGL1Renderer({
+	canvas: document.querySelector('#app'),
+});
+
+const geometry = new THREE.SphereGeometry(10, 64, 32);
+const moonTexture = new THREE.TextureLoader().load('moon.png');
+const moonMap = new THREE.TextureLoader().load('NormalMap.png');
+const material = new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: moonMap });
+const moon = new THREE.Mesh(geometry, material);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 10, 15);
+scene.add(moon);
+scene.add(pointLight);
+
+renderer.setSize(s.width, s.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	moon.rotation.y -= 0.001;
+}
+animate();
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
